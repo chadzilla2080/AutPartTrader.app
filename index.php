81753640cf5750db9dd91e44f71964514103d26b
Chad Buie <chadbuie@gmail.com>
2016-03-10 16:21:48 -0500

    This is the setup of the main index that builds the web application.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+</head>
+<body>
+
+<?php include("/includes/header.inc.php"); ?>
+
+<?php include("/includes/main_content.inc.php"); ?>
+
+<?php include("/includes/footer.inc.php"); ?>
+
+</body>
+</html>
