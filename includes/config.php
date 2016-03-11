1236788bdf3bdbca7d304ff435e7c6ff98924e01
Chad Buie <chadbuie@gmail.com>
2016-03-11 16:15:05 -0500

    Setting up configuration file for the application.

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database Constants
+
+if(!defined('DB_SERVER')) {define("DB_SERVER", "localhost");}
+define("DB_USER", "deploy");
+define("DB_PASS", "Supra2080!!!");
+define("DB_NAME", "autotrader_app");
+
+
+
+
+?>
