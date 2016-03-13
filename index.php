3aa7156503627d4da9044c81bf08628ba1a7339b
Chad Buie <chadbuie@gmail.com>
2016-03-13 16:17:03 -0400

    Loading in a complete database configuration file, set for multiple databases!

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+
+require_once("../includes/database.php");
+
+// here just testing out if the path for the database is working
+// also checking to see if the variables passed to database file
+// are correct
+
+if(isset($database))
+{
+    echo "true";
+}
+
+else
+{
+    echo "false";
+}
+
+
+?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -5,11 +25,6 @@
 </head>
 <body>
 
-<?php include("/includes/header.inc.php"); ?>
-
-<?php include("/includes/main_content.inc.php"); ?>
-
-<?php include("/includes/footer.inc.php"); ?>
 
 </body>
 </html>
